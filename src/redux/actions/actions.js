79acdcc143ebaeccba4dfa137943fa6b7e36e860
Jalal Mallah <jalal_mallah@live.com>
2021-05-17 21:30:48 +0200

    feat: create redux boilerplate

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,9 @@
+import { ACTION_ONE, ACTION_TWO } from './types';
+
+export const anActionCreator = () => ({
+  type: ACTION_ONE,
+});
+
+export const anotherActionCreator = () => ({
+  type: ACTION_TWO,
+});
